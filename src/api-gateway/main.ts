3435645dd0693d1d8e41c5c4d37fa0bb1aee0b2f
ivanGG23 <243836@ids.upchiapas.edu.mx>
2026-04-25 03:17:35 -0600

    correccion de error de conexion

--- src/api-gateway/main.ts
+++ src/api-gateway/main.ts
@@ -4,16 +4,15 @@ import cors from "cors";
 import routes from "./routes/Routes";
 
 const app = express();
-const PORT = process.env.GATEWAY_PORT || 3000;
+const PORT = process.env.GATEWAY_PORT || 3100;
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL || "http://localhost:5173", 'http://localhost:3000'],
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true,
 }));
 
 app.use(express.json());
 
-// Todas las rutas pasan por el gateway
 app.use("/api", routes);
 
 app.listen(PORT, () => {
